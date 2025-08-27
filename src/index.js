fa5c2103c7708637de54597af9aee3d635052a1a
Stian Forren <stian.forren@experisacademy.no>
2025-08-27 13:34:13 +0200

    added imagerotation

--- src/index.js
+++ src/index.js
@@ -1,26 +1,42 @@
+function getSpriteUrls(sprites){
+    let spriteUrl = []
+    function getUrlWithRec(toCheck){
+        for (const key in toCheck){
+            value = toCheck[key]
+            if (value != null && typeof value == "string"){
+                spriteUrl.push(value);
 
-console.log(data);
+            }
+            else if (value != null && typeof value == "object") {
+                getUrlWithRec(value);
+            }
+        }
 
-//You can start simple and just render a single 
-//pokemon card from the first element
-console.log(data[0]);
-// for (i = 0; i<data.length;i++){
-//     const newCard = document.createElement('card');
-//     newCard.
-// }
+    }
+
+    getUrlWithRec(sprites);
+    return spriteUrl
+}
+
+
 let cards = document.getElementById('cards')
-// console.log(cards)
 data.forEach(card => {
+    const spriteUrls = getSpriteUrls(card.sprites)
     const newCard = document.createElement('card');
+    let counter = 0;
     newCard.className = 'card'
+    newCard.id = card.name;
+
     const header = document.createElement('h2');
     header.textContent = card.name;
     newCard.appendChild(header)
+
     const img = document.createElement('img');
     img.className = 'card--img'
     img.src = card.sprites.other["official-artwork"].front_default;
     img.width = 256;
     newCard.appendChild(img);
+
     const cardDetails = document.createElement('ul');
     cardDetails.className = 'card--text'
     card.stats.forEach(stat => {
@@ -28,6 +44,7 @@ data.forEach(card => {
         li.textContent = stat.stat.name + ": " + stat.base_stat
         cardDetails.appendChild(li);
     });
+
     const appearedIn = document.createElement('ul')
     appearedIn.className = 'card--text';
     appearedIn.innerHTML = '<strong> Appeared in </strong>'
@@ -36,7 +53,15 @@ data.forEach(card => {
         li.textContent = gameName.version.name;
         appearedIn.appendChild(li);
     })
+
     newCard.appendChild(cardDetails)
     newCard.appendChild(appearedIn)
+
+    newCard.addEventListener("click", function(){
+        counter++;
+        img.src = spriteUrls[counter];
+    })
+
     cards.appendChild(newCard)
 });
+
